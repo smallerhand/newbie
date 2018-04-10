80f5f2b7e46c46bf2bd7df91e1055e247f89e91a
smallerhand <mrmu0124@gmail.com>
2018-04-10 11:43:42 +0900

    all

---
+++ My_Little_Practice/AND NAND OR GATE_using numpy.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 11:31:48 2018
+
+@author: kimseunghyuck
+"""
+
+import numpy as np
+x=np.array([0,1])       #입력값
+w=np.array([.5, .5])    #가중치값
+b=.7
+w*x
+np.sum(w*x)
+def AND(x1,x2):
+    x=np.array([x1,x2])
+    w=np.array([1,1])
+    b=-1.5
+    tmp=np.sum(w*x)+b
+    return(int(int(tmp>0)))
+
+def NAND(x1,x2):
+    x=np.array([x1,x2])
+    w=np.array([-1,-1])
+    b=1.5
+    tmp=np.sum(w*x)+b
+    return(int(int(tmp>0)))
+
+def OR(x1,x2):
+    x=np.array([x1,x2])
+    w=np.array([1,1])
+    b=0
+    tmp=np.sum(w*x)+b
+    return(int(int(tmp>0)))
+
+lst=[[0,0], [1,0], [0,1], [1,1]]
+
+print("AND")
+for i in lst:
+    print(AND(i[0], i[1]))
+print("NAND")    
+for i in lst:
+    print(NAND(i[0], i[1]))
+print("OR")
+for i in lst:
+    print(OR(i[0], i[1]))
